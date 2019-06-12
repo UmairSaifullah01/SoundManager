e7be1e5feb6c3b589ef6c91f8ff662dc47f0d1ad
UmairSaifullah01 <50673971+UmairSaifullah01@users.noreply.github.com>
2019-06-12 17:26:10 +0500

    Add files via upload

---
+++ AudioPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UMGS;
+public class AudioPlayer : Singleton<AudioPlayer>
+{
+    private AudioSource _speaker;
+    private AudioSource Speaker
+    {
+        get
+        {
+            if (_speaker == null)
+                _speaker = GetComponent<AudioSource> ();
+            if (_speaker == null)
+                _speaker = gameObject.AddComponent<AudioSource> ();
+            return _speaker;
+        }
+    }
+
+
+    public static void Play (AudioClip clip)
+    {
+        instance.Speaker.PlayOneShot (clip);
+    }
+}
